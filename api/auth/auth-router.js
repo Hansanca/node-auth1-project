16e1f85397e5b1314de0aed13aab9fc446aa944e
Hansanca <hansanca@gmail.com>
2022-08-24 10:31:01 -0400

    done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -71,6 +71,8 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
   if (bcrypt.compareSync(password, req.user.password)) {
     req.session.user = req.user
     res.json({ message: `welcome ${req.user.username}`})
+      req.session.user = req.user
+      res.json({ message :`Welcome ${req.user.usernmae}`})
   } else {
     next({ status: 401, message: "Invalid credentials" })
   }
@@ -93,7 +95,17 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
  */
 
 router.get("/logout", (req, res, next) => {
-  res.json("logout");
+  if (req.session.user) {
+    req.session.destroy(err => {
+      if (err) {
+        next(err)
+      } else {
+        res.json({ message: 'logged out'})
+      }
+    })
+  } else {
+    res.json({ message: 'no session'})
+  }
 });
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
